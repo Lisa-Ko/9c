030d4a2e628cccfdc1c804dbb62d8061b324d4be
drkop <satanatyn@yandex.ru>
2023-03-16 09:39:06 +0300

    5

---
+++ ex5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень");
+int B = int.Parse(Console.ReadLine()!);
+int Degree(int A,int B)
+{ 
+    if(B == 0) return 1;
+    else return Degree(A, B-1) * A;
+}
+Console.WriteLine(Degree(A, B));
