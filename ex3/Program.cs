a6498c1305400b9ff5f17adb0262e5553eef80a0
drkop <satanatyn@yandex.ru>
2023-03-16 09:38:41 +0300

    3

---
+++ ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+
+int NumberSum(int n)
+{ 
+   if (n == 0) return 0;
+   return NumberSum(n/10) + n%10;
+}
+Console.WriteLine(NumberSum(n));
