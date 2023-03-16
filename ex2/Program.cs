49f68503c9f7ac9a9b3d1a3a7cc74ceec803c35b
drkop <satanatyn@yandex.ru>
2023-03-16 09:38:25 +0300

    2

---
+++ ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+int Sum(int m,int n)
+{ 
+    if(n == m) return n;
+    else return Sum(m - 1, n) + m;
+}
+Console.WriteLine(Sum(m,n));
