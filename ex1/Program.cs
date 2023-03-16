7904280b774bb54a97590d4ab8b4e7b31a32586b
drkop <satanatyn@yandex.ru>
2023-03-16 09:38:12 +0300

    1

---
+++ ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+string NumberString(int n,int m)
+{ 
+    if(n <= m) return NumberString(n+1, m) + $"{n} ";
+    else return String.Empty;
+}
+Console.WriteLine(NumberString(n, m));
